bfb85fbc7705662f415da4b12ce077bb649ba522
root <root@g0ld3n-b01.home-life.hub>
2020-04-14 19:42:02 +0200

    shopping list update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply text', 'https://www.justataste.com/wp-content/uploads/2014/09/roast-chicken-garlic-lemon-recipe.jpg'),
+    new Recipe('A Test Recipe', 'This is a simply text', 'https://www.justataste.com/wp-content/uploads/2014/09/roast-chicken-garlic-lemon-recipe.jpg')
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient-model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Chicken', 2),
+    new Ingredient('Potato', 5)
+  ];
 
   constructor() { }
 
